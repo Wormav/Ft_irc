d0b33043a048a1fcb78268401c9d97927e5555f8
Jérémy Lorette <jeremy.lorette@outlook.com>
2025-04-07 13:55:28 +0200

    add user in server

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Server.hpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/06 12:47:58 by jlorette          #+#    #+#             */
-/*   Updated: 2025/04/06 13:00:05 by jlorette         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
@@ -17,14 +5,15 @@
 
 class Server
 {
-    public:
-        Server(int port, const std::string& password);
-        void run();
-
     private:
         int port;
         std::string password;
         int server_fd;
+        int epoll_fd; // Ajout de epoll_fd comme membre de la classe
+
+    public:
+        Server(int port, const std::string& password);
+        void run();
 };
 
 #endif
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Server.cpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aauberti <aauberti@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/06 12:48:35 by jlorette          #+#    #+#             */
-/*   Updated: 2025/04/07 08:36:27 by aauberti         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <Server.hpp>
 #include <iostream>
 #include <sys/socket.h>
@@ -17,9 +5,12 @@
 #include <unistd.h>
 #include <cstring>
 #include <cerrno>
+#include <sys/epoll.h>
+#include <vector>
+#include <map>
 
 Server::Server(int port, const std::string& password)
-    : port(port), password(password), server_fd(-1) {}
+    : port(port), password(password), server_fd(-1), epoll_fd(-1) {}
 
 void Server::run()
 {
@@ -63,84 +54,131 @@ void Server::run()
         return;
     }
 
+    // Create epoll instance
+    epoll_fd = epoll_create1(0);
+    if (epoll_fd < 0)
+    {
+        std::cerr << "Error creating epoll instance: " << strerror(errno) << std::endl;
+        close(server_fd);
+        return;
+    }
+
+    // Add server socket to epoll
+    struct epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = server_fd;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event) < 0)
+    {
+        std::cerr << "Error adding server socket to epoll: " << strerror(errno) << std::endl;
+        close(server_fd);
+        close(epoll_fd);
+        return;
+    }
+
     std::cout << "Server started on port " << port << std::endl;
     std::cout << "Waiting for connections..." << std::endl;
 
-    // Loop to accept connections
+    // Main epoll loop
+    const int MAX_EVENTS = 10;
+    struct epoll_event events[MAX_EVENTS];
+    std::map<int, std::string> client_buffers;
+
     while (true)
     {
-        int client_fd;
-        struct sockaddr_in client_addr;
-        socklen_t addr_len = sizeof(client_addr);
-
-        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
-        if (client_fd < 0)
+        int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        if (nfds < 0)
         {
-            std::cerr << "Error accepting connection: " << strerror(errno) << std::endl;
-            continue;
+            std::cerr << "Error during epoll_wait: " << strerror(errno) << std::endl;
+            break;
         }
 
-        std::cout << "New connection accepted!" << std::endl;
-
-        // Send a welcome message to the customer
-        const char* welcome = "220 Welcome to the IRC server!\r\n";
-        send(client_fd, welcome, strlen(welcome), 0);
-
-        // Communication loop with the client
-        char buffer[1024];
-        while (true)
+        for (int i = 0; i < nfds; ++i)
         {
-            memset(buffer, 0, sizeof(buffer));
-            int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-
-            if (bytes_received <= 0)
+            if (events[i].data.fd == server_fd)
             {
-                // Client disconnected or error
-                if (bytes_received == 0)
-                    std::cout << "Client disconnected" << std::endl;
-                else
-                    std::cerr << "Error receiving data: " << strerror(errno) << std::endl;
-                break;
-            }
+                // New connection
+                int client_fd;
+                struct sockaddr_in client_addr;
+                socklen_t addr_len = sizeof(client_addr);
 
-            // // Process the received message
-            // std::cout << "Received: " << buffer;
+                client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+                if (client_fd < 0)
+                {
+                    std::cerr << "Error accepting connection: " << strerror(errno) << std::endl;
+                    continue;
+                }
 
-            // // Echo back the message (simple response)
-            // std::string response = "Server echo: ";
-            // response += buffer;
-            // send(client_fd, response.c_str(), response.length(), 0);
-            std::string msg(buffer);
+                // Add client socket to epoll
+                event.events = EPOLLIN;
+                event.data.fd = client_fd;
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event) < 0)
+                {
+                    std::cerr << "Error adding client socket to epoll: " << strerror(errno) << std::endl;
+                    close(client_fd);
+                    continue;
+                }
 
-            if (msg.find("JOIN") == 0)
-            {
-                std::string channel = msg.substr(5);
-                size_t newline = channel.find_first_of("\r\n");
-                if (newline != std::string::npos)
-                    channel = channel.substr(0, newline);
+                std::cout << "New connection accepted!" << std::endl;
 
-                if (channel[0] != '#')
-                    channel = "#" + channel;
-
-                std::string nick = "aauberti"; // definir l'username correctement apres ! juste je fais des test la 
-                std::string join_reply;
-
-                join_reply += ":" + nick + "!~user@localhost JOIN :" + channel + "\r\n";
-                join_reply += ":ircserv 332 " + nick + " " + channel + " :Welcome to the channel!\r\n";
-                join_reply += ":ircserv 353 " + nick + " = " + channel + " :" + nick + "\r\n";
-                join_reply += ":ircserv 366 " + nick + " " + channel + " :End of /NAMES list.\r\n";
-
-                send(client_fd, join_reply.c_str(), join_reply.length(), 0);
+                // Send a welcome message to the client
+                const char* welcome = "220 Welcome to the IRC server!\r\n";
+                send(client_fd, welcome, strlen(welcome), 0);
             }
             else
             {
-                std::string response = "Server echo: ";
-                response += buffer;
-                send(client_fd, response.c_str(), response.length(), 0);
+                // Data from client
+                int client_fd = events[i].data.fd;
+                char buffer[1024];
+                memset(buffer, 0, sizeof(buffer));
+                int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+
+                if (bytes_received <= 0)
+                {
+                    // Client disconnected or error
+                    if (bytes_received == 0)
+                        std::cout << "Client disconnected" << std::endl;
+                    else
+                        std::cerr << "Error receiving data: " << strerror(errno) << std::endl;
+
+                    close(client_fd);
+                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
+                    continue;
+                }
+
+                std::string msg(buffer);
+                client_buffers[client_fd] += msg;
+
+                if (msg.find("JOIN") == 0)
+                {
+                    std::string channel = msg.substr(5);
+                    size_t newline = channel.find_first_of("\r\n");
+                    if (newline != std::string::npos)
+                        channel = channel.substr(0, newline);
+
+                    if (channel[0] != '#')
+                        channel = "#" + channel;
+
+                    std::string nick = "aauberti"; // Define the correct username here
+                    std::string join_reply;
+
+                    join_reply += ":" + nick + "!~user@localhost JOIN :" + channel + "\r\n";
+                    join_reply += ":ircserv 332 " + nick + " " + channel + " :Welcome to the channel!\r\n";
+                    join_reply += ":ircserv 353 " + nick + " = " + channel + " :" + nick + "\r\n";
+                    join_reply += ":ircserv 366 " + nick + " " + channel + " :End of /NAMES list.\r\n";
+
+                    send(client_fd, join_reply.c_str(), join_reply.length(), 0);
+                }
+                else
+                {
+                    std::string response = "Server echo: ";
+                    response += buffer;
+                    send(client_fd, response.c_str(), response.length(), 0);
+                }
             }
         }
-
-        // Close connection after client disconnects or error
-        close(client_fd);
     }
+
+    // Clean up
+    close(server_fd);
+    close(epoll_fd);
 }
