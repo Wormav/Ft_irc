8982743197d5c13294508668dde5b1c1bc132cc5
Jeremy lorette <jeremy.lorette@outlook.com>
2025-04-09 10:50:58 +0200

    add class cmd and basic cmd

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -6,6 +6,7 @@
 #include <set>
 #include "User.hpp"
 #include "Channel.hpp"
+#include "Command.hpp"
 
 class Server
 {
@@ -14,34 +15,24 @@ private:
     std::string password;
     int server_fd;
     int epoll_fd;
-    
+
     std::map<int, std::string> client_buffers;
     std::map<int, User> users;
     std::map<std::string, Channel> channels;
-    
+    Command* command_handler;
+
     // Méthodes privées pour gérer le serveur
     void setupSocket();
     void handleNewConnection();
     void handleClientData(int client_fd);
-    void disconnectClient(int client_fd);
     void processCommand(int client_fd, const std::string& line);
-    void sendWelcomeMessages(int client_fd, const User& user);
 
 public:
     Server(int port, const std::string& password);
     ~Server();
-    
-    void run();
 
-    // Méthodes pour gérer les commandes
-    void handlePass(int client_fd, std::istringstream& iss);
-    void handleNick(int client_fd, std::istringstream& iss);
-    void handleUser(int client_fd, const std::string& line);
-    void handleJoin(int client_fd, std::istringstream& iss);
-    void handlePrivmsg(int client_fd, std::istringstream& iss, bool isNotice);
-    void handlePing(int client_fd, std::istringstream& iss);
-    void handlePart(int client_fd, const std::string& line);
-    void handleQuit(int client_fd, std::istringstream& iss);
+    void run();
+    void disconnectClient(int client_fd);
 };
 
 #endif
